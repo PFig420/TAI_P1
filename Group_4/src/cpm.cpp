ec345c787f31cfcd43a37a75de9eb304c5cfb47b
Renatito122 <renatoaldias12@ua.pt>
2023-03-17 23:06:08 +0000

    Suggestions implemented/fixed

--- Group_4/src/cpm.cpp
+++ Group_4/src/cpm.cpp
@@ -12,24 +12,20 @@ struct WordData {
     int fails;
     double prob;
     char nextSymb;
+    int num_consecutive_fails;
 };
 
 int main(int argc, char* argv[])
 {
     if (argc != 5) {
-        cout << "Usage: " << argv[0] << " <filename> <word_length> <threshold> <min_prob>" << endl;
+        cout << "Usage: " << argv[0] << " <filename> <word_length> <threshold> <max_num_fails>" << endl;
         return 1;
     }
 
     string filename = argv[1];
     int word_length = stoi(argv[2]);
     double threshold = stod(argv[3]);
-    double min_prob = stod(argv[4]);
-
-    if (min_prob >= 0.5) {
-        cout << "Error: min_prob value must be less than 0.5" << endl;
-        return 1;
-    }
+    int max_num_fails = stoi(argv[4]);
 
     ifstream file(filename);
     if (!file) {
@@ -54,16 +50,17 @@ int main(int argc, char* argv[])
             // Word found in map, update values
             if (sequence[i + word_length] == word_map[word].nextSymb) {
                 word_map[word].hits++;
+                word_map[word].num_consecutive_fails = 0;
             } else {
                 word_map[word].fails++;
+                word_map[word].num_consecutive_fails++;
             }
-            word_map[word].prob = static_cast<double>(word_map[word].hits + threshold) / (word_map[word].hits + word_map[word].fails + 2*threshold);
-            word_map[word].nextSymb = sequence[i + word_length];
-        }
-
-        // Check if probability is lower than minimum and remove entry if needed
-        if (word_map[word].prob < min_prob) {
-            word_map.erase(word);
+            if (word_map[word].num_consecutive_fails >= max_num_fails) {
+                word_map.erase(word);
+            } else {
+                word_map[word].prob = static_cast<double>(word_map[word].hits + threshold) / (word_map[word].hits + word_map[word].fails + 2 * threshold);
+                word_map[word].nextSymb = sequence[i + word_length];
+            }
         }
     }
 
@@ -83,7 +80,8 @@ int main(int argc, char* argv[])
         cout << it->first << ": hits = " << it->second.hits
              << ", fails = " << it->second.fails
              << ", prob = " << it->second.prob
-             << ", nextSymb = " << it->second.nextSymb << endl;
+             << ", nextSymb = " << it->second.nextSymb
+             << ", num_consecutive_fails = " << it->second.num_consecutive_fails << endl;
     }
 
     double total_bits = 0.0;
@@ -91,14 +89,13 @@ int main(int argc, char* argv[])
     cout << "\nList of symbols and their associated probabilities:" << endl;
     for (auto it = symbol_counts.begin(); it != symbol_counts.end(); it++) {
         double prob = static_cast<double>(it->second) / total_symbols;
-        total_bits += prob * log2(1.0 / prob);
+        total_bits += -log2(prob);
         cout << it->first << ": " << prob << endl;
     }
 
     double avg_bits_per_symbol = total_bits / total_symbols;
-    double total_num_bits = avg_bits_per_symbol * total_symbols;
 
-    cout << "\nEstimated total number of bits: " << total_num_bits << endl;
+    cout << "\nEstimated total number of bits: " << total_bits << endl;
     cout << "Average number of bits per symbol: " << avg_bits_per_symbol << endl;
 
     return 0;
